c06377647f8eeb322579d9836c0c60e80062c6cd
technokowski <gabrielkavaloski@gmail.com>
2018-01-29 15:54:40 -0800

    bs4 import

--- src/app.py
+++ src/app.py
@@ -1,10 +1,17 @@
 __author__ = "technokowski"
 
 import requests
+from bs4 import BeautifulSoup
 
-request = requests.get("http://www.google.com")
+request = requests.get('https://www.amazon.com/Nintendo-New-3DS-XL-Black/dp/B00S1LRX3W')
+content = request.content
+soup = BeautifulSoup(content, "html.parser")
+element = soup.find("span", {"class": "buyingPrice"})
+print(element.text.strip())
+# <span class="buyingPrice">194</span>
 
-def ask_age():
+'''
+ def ask_age():
     age = input("Enter your age: ")
     return int(age)
 
@@ -15,7 +22,7 @@ def prompt_user_and_calculate_age():
     age = ask_age()
     seconds_lived = calcuate_seconds_from_years(age)
     print("You have lived for {} seconds".format(seconds_lived))
+'''
 
 #prompt_user_and_calculate_age()
 
-print(request.content)
