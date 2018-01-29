36f677656d6f123baad803c08022839a8ba7315a
technokowski <gabrielkavaloski@gmail.com>
2018-01-29 13:26:48 -0800

    first phase; creating funcs

---
+++ app.py
@@ -0,0 +1,17 @@
+__author__ = 'technokowski'
+
+
+def ask_age():
+    age = input("Enter your age: ")
+    return int(age)
+
+def calcuate_seconds_from_years(age_years):
+    return age_years * 365 * 24 * 3600
+
+def prompt_user_and_calculate_age():
+    age = ask_age()
+    seconds_lived = calcuate_seconds_from_years(age)
+    print("You have lived for {} seconds".format(seconds_lived))
+
+prompt_user_and_calculate_age()
+
